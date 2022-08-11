596b12389b9aaf9265638ef800d342eecebc7c29
Sergey Novozhilov <sergionov@mail.ru>
2022-08-11 18:28:02 +0300

    Modify files

--- static/nephrocenter/js/common.js
+++ static/nephrocenter/js/common.js
@@ -1,38 +1,20 @@
 const btnBurger = document.getElementById('btn-burger');
 
+const __get__ = (selector => {
+    return document.getElementById(selector);
+})
+
 btnBurger.addEventListener('click', event => {
-    const headerLogoMenu = document.querySelector('#header-logo__menu');
+    const headerLogoMenu = __get__('#header-logo__menu');
     headerLogoMenu.setAttribute('style', 'display: flex');
 
-    const btnClose = document.getElementById('btn-close');
+    const btnClose = __get__('#btn-close');
 
     btnClose.addEventListener('click', e => {
         headerLogoMenu.setAttribute('style', 'display: none');
     });
 
-    headerLogoMenu.addEventListener('click', e => {
-        // console.log(`Кнопка: ${e.which}`);
-        // console.log(`Координаты: (${e.x};${e.y})`);
-        // console.log(`Тип события: ${e.type}`);
-        // console.log(`Тег элемента: ${e.currentTarget.tagName}`);
-
-        // console.log(e.target);
-        // console.log({ event: typeof e.target });
-
-        // if (e.target === 'a') {
-        //     console.log({ event: e.target });
-        // }
-
-        // headerLogoMenu.setAttribute('style', 'display: none');
-
-        // headerLogoMenu.classList.remove('show');
-        // headerLogoMenu.classList.add('hide');
-    });
-
-    // headerLogoMenu.classList.remove('hide');
-    // headerLogoMenu.classList.add('show');
-
-    console.log({ headerLogoMenu });
+    // console.log({ headerLogoMenu });
 })
 
 
