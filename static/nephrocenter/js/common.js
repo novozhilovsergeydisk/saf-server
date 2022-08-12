c63f7af4843c7d3584bc201589a4d2aa19ecd07e
Sergey Novozhilov <sergionov@mail.ru>
2022-08-12 18:24:16 +0300

    Modify files

--- static/nephrocenter/js/common.js
+++ static/nephrocenter/js/common.js
@@ -1,20 +1,16 @@
 const btnBurger = document.getElementById('btn-burger');
 
-const __get__ = (selector => {
-    return document.getElementById(selector);
-})
-
 btnBurger.addEventListener('click', event => {
-    const headerLogoMenu = __get__('#header-logo__menu');
+    const headerLogoMenu = document.querySelector('#header-logo__menu');
     headerLogoMenu.setAttribute('style', 'display: flex');
 
-    const btnClose = __get__('#btn-close');
+    const btnClose = document.getElementById('btn-close');
 
     btnClose.addEventListener('click', e => {
         headerLogoMenu.setAttribute('style', 'display: none');
     });
 
-    // console.log({ headerLogoMenu });
+    console.log({ headerLogoMenu });
 })
 
 
